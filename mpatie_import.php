3ac3b265ba3b8e63b2ff3c7df2416ad8dd8763cc
Jean-Claude Dufourd <jean-claude.dufourd@telecom-paristech.fr>
2017-11-14 14:20:52 +0100

    copyright header

--- mpatie_import.php
+++ mpatie_import.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ *
+ * Copyright (c) 2017 MPAT Consortium , All rights reserved.
+ * Fraunhofer FOKUS, Fincons Group, Telecom ParisTech, IRT, Lacaster University, Leadin, RBB, Mediaset
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ * Aurelien David https://github.com/aureliendavid/
+ * Jean-Claude Dufourd (jean-claude.dufourd@telecom-paristech.fr)
+ **/
 namespace MPAT\ImportExport;
 
 use MPAT\ImportExport\ImportExport;
@@ -8,7 +30,7 @@ $nullGuard = null;
 function getcontents($postType, $zipped){
 	$json = null;
 	$data = file_get_contents( $_FILES[$postType]['tmp_name']);
-	if($zipped) {	
+	if($zipped) {
 		set_error_handler(function() {});
 		$json = gzuncompress($data);
 		restore_error_handler();
